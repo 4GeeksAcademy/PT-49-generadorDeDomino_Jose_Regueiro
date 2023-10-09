effe3f3ceb43b8d2eeaf9773fcf5367641350025
JoseRegueiro17 <132882721+JoseRegueiro17@users.noreply.github.com>
2023-10-09 19:07:30 +0000

    agregando los loops

--- src/app.js
+++ src/app.js
@@ -6,6 +6,19 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our", "my"];
+  let adj = ["great", "big", "happy"];
+  let noun = ["jogger", "racoon", "chigüire"];
+  let dom = [".com", ".es", ".edu"];
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        for (let l = 0; l < dom.length; l++) {
+          let domainNewName = "www." + pronoun[i] + adj[j] + noun[k] + dom[l];
+          console.log(domainNewName);
+        }
+      }
+    }
+  }
 };
